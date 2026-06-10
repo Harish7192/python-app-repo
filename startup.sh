eccec32c4cfd146a8195bb44a40824b0d94f438d
Harish <bbharishkumar7192@gmail.com>
2026-06-10 05:39:36 +0000

    Commit Done

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 1. Authenticate Docker locally inside the VM
+gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
+
+# 2. Pull the newly pushed image
+docker pull us-central1-docker.pkg.dev/project-ee16ab9f-709f-4851-b92/cloud-handson-v2/my-app:latest
+
+# 3. Clean up any stale existing container instances
+docker stop my-running-app || true
+docker rm my-running-app || true
+
+# 4. Start up the brand new container deployment
+docker run -d --name my-running-app -p 80:80 us-central1-docker.pkg.dev/project-ee16ab9f-709f-4851-b92/cloud-handson-v2/my-app:latest
